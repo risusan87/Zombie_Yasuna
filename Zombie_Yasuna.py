d232af6d4eb8eb87fdb0686998e248dc453fd46f
Shiratama_Risu <risusan8795@gmail.com>
2021-08-02 16:52:27 +0900

    setting up event listeners

--- Zombie_Yasuna.py
+++ Zombie_Yasuna.py
@@ -1,2 +1,30 @@
 import discord
+from discord.ext import commands
+from utils.JsonIO import JsonIO
 
+private_keys = JsonIO('data/private_keys.json').read().result()
+client = commands.Bot(command_prefix=private_keys['prefix'])
+
+
+@client.event
+async def on_ready():
+    await init()
+    print('ready')
+
+
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+
+
+@client.command(name='help')
+async def _help(ctx, *args):
+    await ctx.channel.send('Hello')
+
+
+async def init(*args, **kwargs):
+    pass
+
+
+def run():
+    client.run(private_keys['bot_token'])
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import json
 import Zombie_Yasuna
+from utils.JsonIO import JsonIO
+
 
 if __name__ == '__main__':
+    Zombie_Yasuna.run()
+
