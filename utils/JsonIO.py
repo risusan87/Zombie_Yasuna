1fb6831f7edc8bc3e43a2c3c60da777bf2b71715
Shiratama_Risu <risusan8795@gmail.com>
2021-08-02 16:21:19 +0900

    FileIO now fully synchronized

--- utils/JsonIO.py
+++ utils/JsonIO.py
@@ -20,20 +20,45 @@ class JsonIO:
         """
         def _i():
             with open(mode='r', file=self.file_path) as file_f:
-                return json.load(file_f)
+                _data = json.load(file_f)
+                file_f.close()
+                return _data
 
         return self.thread_pool.submit(_i)
 
-    def write(self, data: dict) -> Future:
-        """ファイルの書き込みを行います。
+    def appendWrite(self, data: dict) -> Future:
+        """データの付け足しを行います。
         戻り値からresult()を呼ぶことで処理の同期待ちをします。
         :param data:
-        Data in dictionary object to be over-written
+        Data in dictionary object to be appended
         :return:
         Future object
         """
         def _o():
+            with open(mode='r', file=self.file_path) as file_f:
+                old_data = json.load(file_f)
+                file_f.close()
             with open(mode='w', file=self.file_path) as file_f:
-                json.dump(data, file_f, indent=4)
+                for k in data.keys():
+                    old_data[k] = data[k]
+                json.dump(old_data, file_f, indent=4)
+                file_f.close()
+                return
 
         return self.thread_pool.submit(_o)
+
+    def overwrite(self, data: dict) -> Future:
+        """データの上書きを行います。
+        元々のデータは消えるので１から書き直すときのみ使用します。
+        戻り値からresult()を呼ぶことで処理の同期待ちをします。
+        :param data:
+        Data to be over-written
+        :return:
+        """
+        def _ow():
+            with open(mode='w', file=self.file_path) as file_f:
+                json.dump(data, file_f, indent=4)
+                file_f.close()
+                return
+
+        return self.thread_pool.submit(_ow)
