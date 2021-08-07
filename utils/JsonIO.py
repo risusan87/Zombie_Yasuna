3d2d83c512f372e0daf36bcec362c5fe2913e8fa
Shiratama_Risu <risusan8795@gmail.com>
2021-08-07 23:26:31 +0900

    made JsonIO.write() to work

--- utils/JsonIO.py
+++ utils/JsonIO.py
@@ -1,4 +1,5 @@
 import json
+from itertools import filterfalse
 from concurrent.futures import ThreadPoolExecutor, Future
 
 
@@ -21,44 +22,55 @@ class JsonIO:
         def _i():
             with open(mode='r', file=self.file_path) as file_f:
                 _data = json.load(file_f)
-                file_f.close()
                 return _data
 
         return self.thread_pool.submit(_i)
 
-    def appendWrite(self, data: dict) -> Future:
-        """データの付け足しを行います。
-        戻り値からresult()を呼ぶことで処理の同期待ちをします。
-        :param data:
-        Data in dictionary object to be appended
-        :return:
-        Future object
-        """
+    def write(self, data: dict, removeMode=False, forceWrite=False, overwrite=False) -> Future:
+        if overwrite:
+            def _ow():
+                with open(mode='w', file=self.file_path) as file_f:
+                    json.dump(data, file_f, indent=4)
+            return self.thread_pool.submit(_ow)
+
+        def _appendThrough(old_data: dict, new_data: dict) -> dict:
+            #_=元々あったやつ
+            fin_dic = old_data
+            for key in new_data:
+                if key in old_data and type(old_data[key]) is type(new_data[key]) if not forceWrite else True:
+                    if type(new_data[key]) is dict:
+                        if removeMode and len(new_data[key]) == 0:
+                            del fin_dic[key]
+                            continue
+                        fin_dic[key] = _appendThrough(old_data[key], new_data[key])
+                    elif type(new_data[key]) is list:
+                        if removeMode:
+                            if len(new_data[key]) == 0:
+                                del fin_dic[key]
+                                continue
+                            else:
+                                n_list = []
+                                for e in old_data[key]:
+                                    if e not in new_data[key]:
+                                        n_list.append(e)
+                                fin_dic[key] = n_list
+                                continue
+                        o_list = old_data[key]
+                        o_list.extend(new_data[key])
+                        fin_dic[key] = o_list
+                    else:
+                        if removeMode:
+                            del fin_dic[key]
+                            continue
+                        fin_dic[key] = new_data[key] if forceWrite else old_data[key]
+                else:
+                    if removeMode:
+                        continue
+                    fin_dic[key] = new_data[key]
+            return fin_dic
         def _o():
             with open(mode='r', file=self.file_path) as file_f:
-                old_data = json.load(file_f)
-                file_f.close()
+                o_data = json.load(file_f)
             with open(mode='w', file=self.file_path) as file_f:
-                for k in data.keys():
-                    old_data[k] = data[k]
-                json.dump(old_data, file_f, indent=4)
-                file_f.close()
-                return
-
+                json.dump(_appendThrough(old_data=o_data, new_data=data), file_f, indent=4)
         return self.thread_pool.submit(_o)
-
-    def overwrite(self, data: dict) -> Future:
-        """データの上書きを行います。
-        元々のデータは消えるので１から書き直すときのみ使用します。
-        戻り値からresult()を呼ぶことで処理の同期待ちをします。
-        :param data:
-        Data to be over-written
-        :return:
-        """
-        def _ow():
-            with open(mode='w', file=self.file_path) as file_f:
-                json.dump(data, file_f, indent=4)
-                file_f.close()
-                return
-
-        return self.thread_pool.submit(_ow)
