1f2c20d9f507c13c14959fe4b1dbc8a85c66ade6
BuildTools <unconfigured@null.spigotmc.org>
2021-08-24 15:28:56 -0400

    added online status

--- Zombie_Yasuna.py
+++ Zombie_Yasuna.py
@@ -1,10 +1,12 @@
+import asyncio
+
 import discord
 import requests
 import time
 import math
 from requests import request
 from mcuuid import MCUUID
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.ext.commands.errors import CommandNotFound
 from utils.JsonIO import JsonIO
 
@@ -16,21 +18,73 @@ debugging = False
 @client.event
 async def on_ready():
     await init()
+    update_online_status.start()
+    await client.change_presence(activity=discord.Game(name="Alien Arcadium solo"))
     print('ready')
 
 
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.channel.id == 877512494468444211 and not message.content.startswith('!yasuna mcid'):
+        await queue_delete(message)
+        return
     await client.process_commands(message)
 
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, CommandNotFound):
+        if ctx.channel.id == 877512494468444211:
+            return
         await ctx.channel.send('Hey Hey! Please type in so that I understand.'
                                ' To see the list of commands, type `!yasuna help`')
         return
     raise error
+
+
+# Task
+@tasks.loop(seconds=60)
+async def update_online_status():
+    mcid_io = JsonIO('data/mcid.json')
+    mcid_data = mcid_io.read().result()
+    embed_id = JsonIO('data/profile.json').read().result()['online_embed_id']
+    t_channel = client.get_channel(id=877512494468444211)
+    embed = discord.Embed(title='Current online status of players')
+    if len(mcid_data) == 0:
+        embed.add_field(
+            name='O-no!',
+            value='Looks like theres noone in the list of mcid!'
+        )
+
+    for str_usr_id in mcid_data:
+        player_stats = requests.get(
+            url='https://api.hypixel.net/status',
+            params={
+                'key': private_keys['hypixel_api_key'],
+                'uuid': mcid_data[str_usr_id]['uuid']
+            }
+        ).json()
+        this_guild = client.get_guild(id=740308010517135470)
+        online_role = discord.utils.get(this_guild.roles, name='Online')
+        offline_role = discord.utils.get(this_guild.roles, name='Offline')
+        try:
+            status = online_role if player_stats['session']['online'] else offline_role
+        except:
+            status = offline_role
+        try:
+            user = await client.fetch_user(int(str_usr_id))
+        except:
+            user = str_usr_id
+        embed.add_field(
+            name=mcid_data[str_usr_id]['name'],
+            value=f'{user if isinstance(user, str) else user.mention} - {status.mention}',
+            inline=False
+        )
+    t_embed = await t_channel.fetch_message(embed_id)
+    await t_embed.edit(embed=embed)
+    pass
 
 
 # COMMANDS
@@ -51,6 +105,104 @@ async def _help(ctx, *args):
     await ctx.channel.send(embed=help_embed)
 
 
+@client.command()
+async def mcid(ctx, *args):
+    t_channel = client.get_channel(id=877512494468444211)
+    if ctx.channel.id != t_channel.id:
+        await ctx.message.delete()
+        return
+    mcid_io = JsonIO('data/mcid.json')
+    if len(args) > 0:
+        mcid_data = mcid_io.read().result()
+        if args[0] == 'set' and len(args) > 1:
+            if str(ctx.author.id) in mcid_data:
+                mess = await t_channel.send(f'You already set your mcid as {mcid_data[str(ctx.author.id)]["name"]}. \n'
+                                            f'Type `!yasuna mcid remove` to remove mcid you set')
+                await queue_delete(mess, ctx.message)
+                return
+            mc_player = MCUUID(name=args[1])
+            try:
+                mc_uuid = mc_player.uuid
+                mc_name = mc_player.name
+            except:
+                mess = await t_channel.send(f'Player {args[1]} could not be found :(')
+                await queue_delete(mess, ctx.message)
+                return
+            mcid_io.write(
+                data={
+                    str(ctx.author.id): {
+                        "name": mc_name,
+                        "uuid": mc_uuid
+                    }
+                }
+            ).result()
+            mess = await t_channel.send(f'Your mcid is set as {mc_name}')
+            await queue_delete(mess, ctx.message)
+            return
+        elif args[0] == 'remove':
+            if str(ctx.author.id) not in mcid_data:
+                mess = await t_channel.send('You have not set your mcid yet.\n'
+                                            'Type `!yasuna mcid set <your_mcid>` to set your mcid')
+                await queue_delete(mess, ctx.message)
+                return
+            mcid_io.write(data={
+                str(ctx.author.id): -1
+            },
+            removeMode=True).result()
+            mess = await t_channel.send('Your mcid is removed.')
+            await queue_delete(mess, ctx.message)
+            return
+        elif args[0] == 'force':
+            if discord.utils.get(ctx.guild.roles, name='admin') not in ctx.author.roles:
+                await queue_delete(ctx.message)
+                return
+            if len(args) > 2:
+                mc_player = MCUUID(name=args[2])
+                try:
+                    mc_name = mc_player.name
+                    mc_uuid = mc_player.uuid
+                except:
+                    mess = await t_channel.send(f'Player {args[2]} could not be found.')
+                    await queue_delete(mess, ctx.message)
+                    return
+                if args[1] == 'add':
+                    if mc_name in mcid_data:
+                        mess = await t_channel.send(f'Player {mc_name} is already in the list.')
+                        await queue_delete(mess, ctx.message)
+                        return
+                    mcid_io.write(
+                        data={
+                            mc_name: {
+                                'name': mc_name,
+                                'uuid': mc_uuid
+                            }
+                        }
+                    ).result()
+                    mess = await t_channel.send(f'{mc_name} is added')
+                    await queue_delete(mess, ctx.message)
+                    return
+                elif args[1] == 'remove':
+                    if mc_name not in mcid_data:
+                        mess = await t_channel.send(f'Player {mc_name} is not in the list.')
+                        await queue_delete(mess, ctx.message)
+                        return
+                    mcid_io.write(
+                        data={
+                            mc_name: -1
+                        },
+                        removeMode=True
+                    ).result()
+                    mess = await t_channel.send(f'{mc_name} is removed.')
+                    await queue_delete(mess, ctx.message)
+                    return
+            mess = await t_channel.send('Usage: `!yasuna mcid force <add|remove> <mcid>`')
+            await queue_delete(mess, ctx.message)
+            return
+    mess = await t_channel.send('Usage: `!yasuna mcid <set|remove> [your_mcid]`')
+    await queue_delete(mess, ctx.message)
+    return
+
+
 @client.command(name='get')
 async def channel_id(ctx, *args):
     if not len(args) >= 2:
@@ -58,15 +210,47 @@ async def channel_id(ctx, *args):
         return
     if args[0] == 'channel' and args[1] == 'id':
         await ctx.channel.send('`{}` is the id for this channel'.format(ctx.channel.id))
+    elif args[0] == 'guild' and args[1] == 'id':
+        await ctx.channel.send(f'{ctx.guild.id} is the id for this guild')
     else:
         await ctx.channel.send('Usage: `!yasuna get channel id`')
+
+
+@client.command()
+async def role(ctx, *args):
+    if len(args) > 1:
+        if args[1].lower() in ['de', 'deadend']:
+            t_role = discord.utils.get(ctx.guild.roles, name='Dead End')
+        elif args[1].lower() in ['bb', 'badblood']:
+            t_role = discord.utils.get(ctx.guild.roles, name='Bad Blood')
+        elif args[1].lower() in ['aa', 'alienarcadium']:
+            t_role = discord.utils.get(ctx.guild.roles, name='Alien Arcadium')
+        elif args[1].lower() in ['zombiezzz']:
+            t_role = discord.utils.get(ctx.guild.roles, name='zombiezzz')
+        elif args[1].lower() in ['skyblocker']:
+            t_role = discord.utils.get(ctx.guild.roles, name='Skyblocker')
+        else:
+            await ctx.channel.send('Please specify one of: DE, BB, AA, or zombiezzz')
+        if args[0] == 'add':
+            if t_role in ctx.author.roles:
+                await ctx.channel.send('I think you already have that role')
+                return
+            await ctx.author.add_roles(t_role)
+            await ctx.channel.send(f'@{t_role.name} is added to {ctx.author.mention}')
+        if args[0] == 'remove':
+            if t_role not in ctx.author.roles:
+                await ctx.channel.send('You don\'t have the role to remove.')
+                return
+            await ctx.author.remove_roles(t_role)
+            await ctx.channel.send(f'@{t_role.name} is now removed from {ctx.author.mention}')
+    else:
+        await ctx.channel.send('Usage: `!yasuna role <add|remove> <de|bb|aa|zombiezzz>`')
 
 
 @client.command()
 async def search(ctx, *args):
     if len(args) == 0:
         await ctx.channel.send('Hey hey! please tell me on which player to look for!')
-        await debug('Process done with an error. Returning null', ctx.channel)
         return
     embed = discord.Embed()
     mc_player = MCUUID(name=args[0])
@@ -204,8 +388,10 @@ async def search(ctx, *args):
             )
     await ctx.channel.send(embed=embed)
 
+
 @client.command()
 async def skyblock(ctx, *args):
+    await ctx.channel.send('Fetching from api.')
     dat = requests.get(
         url='https://api.hypixel.net/resources/skyblock/collections',
         params={
@@ -216,26 +402,22 @@ async def skyblock(ctx, *args):
 ##
 
 
-async def debug_start(starting_mess: str, channel: discord.TextChannel):
-    if debugging:
-        await channel.send(f'`Debug: {starting_mess}`')
-        return time.time()
-
-
-async def debug_end(end_mess: str, channel: discord.TextChannel, timestamp: float):
-    if debugging:
-        time_took = round((time.time() - timestamp) * 1000, 2)
-        await channel.send(f'`Debug: {end_mess} (took {time_took} millis)`')
-
-
-async def debug(mess: str, channel: discord.TextChannel):
-    if debugging:
-        await channel.send(f'`Debug: {mess}`')
+async def queue_delete(*messages):
+    await asyncio.sleep(10)
+    for m in messages:
+        await m.delete()
 
 
 async def init(*args, **kwargs):
-    pass
+    profile_io = JsonIO('data/profile.json')
+    online_embed = discord.Embed(title='Online')
+    online_channel = client.get_channel(id=877512494468444211)
+    oe_id = await online_channel.send(embed=online_embed)
 
+    fin_data = {
+        'online_embed_id': oe_id.id
+    }
+    profile_io.write(data=fin_data, forceWrite=True).result()
 
 def run():
     client.run(private_keys['bot_token'])
